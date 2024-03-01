c6b3d0b9e36f5273e19f5d4e2488a3f065d88e4a
Justin <park.justin.ib@gmail.com>
2024-03-01 14:38:36 -0800

    Initial commit with Stories and Flows apps

---
+++ TechChallengeAppStories/actions/actions.py
@@ -0,0 +1,15 @@
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+import random
+
+
+class ActionCheckStatus(Action):
+
+    def name(self):
+        return "action_check_status"
+
+    def run(self, dispatcher, tracker, domain):
+        # return a random status, just a mockup
+        statuses = ["received", "rejected", "interview", "unknown"]
+        status = random.choice(statuses)
+        return [SlotSet("status", status)]
